1f3c7a4e5ff4c7aa32de753e37ab36cd5bff2738
Swastik Biswas <swastikbiswas962@gmail.com>
2025-12-23 12:20:06 +0530

    Add dummy header to resolve MinGW installation issue

---
+++ src/avx512vbmi2intrin.h
@@ -0,0 +1,7 @@
+/* 
+ * Dummy header to fix broken MinGW installation.
+ * This file is intentionally empty to bypass the missing avx512vbmi2intrin.h error.
+ */
+#ifndef _AVX512VBMI2INTRIN_H_INCLUDED
+#define _AVX512VBMI2INTRIN_H_INCLUDED
+#endif
