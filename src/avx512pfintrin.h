7138eb94c2f164da23afcb6994c208f4ace250bc
Swastik Biswas <swastikbiswas962@gmail.com>
2025-12-23 12:20:00 +0530

    Add dummy header to resolve MinGW installation issue

---
+++ src/avx512pfintrin.h
@@ -0,0 +1,7 @@
+/* 
+ * Dummy header to fix broken MinGW installation.
+ * This file is intentionally empty to bypass the missing avx512pfintrin.h error.
+ */
+#ifndef _AVX512PFINTRIN_H_INCLUDED
+#define _AVX512PFINTRIN_H_INCLUDED
+#endif
